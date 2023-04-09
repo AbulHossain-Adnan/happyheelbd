e569b60dc2534f1a6976e8e645ae37893252b487
adnan <alhossain.a92@gmail.com>
2023-04-09 23:06:30 +0600

    worked on full project

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -89,27 +89,7 @@
 </div>
 <!-- END LOGIN SECTION -->
 
-<!-- START SECTION SUBSCRIBE NEWSLETTER -->
-<div class="section bg_default small_pt small_pb">
-    <div class="container"> 
-        <div class="row align-items-center">    
-            <div class="col-md-6">
-                <div class="heading_s1 mb-md-0 heading_light">
-                    <h3>Subscribe Our Newsletter</h3>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="newsletter_form">
-                    <form>
-                        <input type="text" required="" class="form-control rounded-0" placeholder="Enter Email Address">
-                        <button type="submit" class="btn btn-dark rounded-0" name="submit" value="Submit">Subscribe</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- START SECTION SUBSCRIBE NEWSLETTER -->
+
 
 </div>
 <!-- END MAIN CONTENT -->
